9b19ff21d077d837172ba5c711ac694b0a3ae78b
kelypso <helloklow@gmail.com>
2020-04-12 19:20:14 -0600

    add meals resource

---
+++ db/migrate/20200413001547_create_meals.rb
@@ -0,0 +1,14 @@
+class CreateMeals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :meals do |t|
+      t.date :date
+      t.string :meal
+      t.string :ingredients
+      t.string :nutrition
+      t.text, :notes
+      t.string :target
+
+      t.timestamps
+    end
+  end
+end
