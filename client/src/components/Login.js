4f915a9471248f0e9d7da22c99890e9ebf5681ab
kelypso <helloklow@gmail.com>
2020-05-10 14:23:14 -0600

    begin login form component

---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {connect} from "react-redux"
+
+const Login = () => {
+    return (
+        <form>
+            <input type="text" name="email" value={email} placeholder="Email" className="form" /><br/>
+            <input type="password" name="password" value={password} placeholder="Password" className="form" /><br/>
+            <input type="submit" value="Log In" className="form" />
+        </form>
+    )
+}
