d6b55f31dda51c8539231bf6acd2483976eb74d6
kelypso <helloklow@gmail.com>
2020-05-10 14:38:41 -0600

    build out login component with form state; add login action to user actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -5,3 +5,26 @@ export const setCurrentUser = user => {
         user
     }
 }
+
+// Asynch action creators (returns dispatch fn in order to fetch from API)
+const api_url = "http://localhost:3001/api/v1"
+
+export const login = (user) => {
+    return dispatch => {
+        return fetch(`${api_url}/login`, {
+            credentials: "include",
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    alert(res.error)
+                } else {
+                    dispatch(setCurrentUser(res.data))
+                }
+            })
+            .catch(err => {return err})
+    }
+}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import {connect} from "react-redux"
+import {login} from '../actions/userActions'
 
-const Login = () => {
+const Login = ({loginData, login, updateLoginForm}) => {
+    const {email, password} = loginData
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        updateLoginForm(name, value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        login(loginData)
+    }
+
     return (
-        <form>
-            <input type="text" name="email" value={email} placeholder="Email" className="form" /><br/>
-            <input type="password" name="password" value={password} placeholder="Password" className="form" /><br/>
-            <input type="submit" value="Log In" className="form" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="email" onChange={handleChange} value={email} placeholder="Email" className="form" /><br/>
+            <input type="password" name="password" onChange={handleChange} value={password} placeholder="Password" className="form" /><br/>
+            <input type="submit" value="Log In" className="form-btn" />
         </form>
     )
 }
 
-export default Login
+const mapStateToProps = state => {
+    return {
+        loginData: state.login
+    }
+}
+
+export default connect(mapStateToProps, {login})(Login)
